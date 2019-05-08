dec5f7ac63bacf81d460477f2c553ce3f87c7a7a
RafaelaSilveira <rafaelacss21@gmail.com>
2019-05-08 16:50:37 -0400

    implement owl-carousel

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
 window.addEventListener("load", () =>
   document.querySelector(".preloader").classList.add("hidePreloader")
 );
+
+$(".owl-carousel").owlCarousel({
+  autoplay: true,
+  dots: false,
+  nav: true,
+  loop: true,
+  navText: [
+    '<i class="fa fa-angle-left"></i>',
+    '<i class="fa fa-angle-right"></i>'
+  ],
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 2
+    }
+  }
+});
